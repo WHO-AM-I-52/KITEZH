61e5417dbe97b36d27eb99816682414f973a745e
WHO-AM-I-52 <toxacull@gmail.com>
2026-05-24 22:47:21 +0300

    fix: peredat put k bat cherez env-peremennuyu, ubrat konflik kavychek v PS

--- start SONAR.bat
+++ start SONAR.bat
@@ -4,8 +4,9 @@ chcp 65001 >nul
 cd /d "%~dp0"
 title SONAR
 
-:: Autofix LF->CRLF (bez perezapuska)
-powershell -Command "$f='%~f0'; $c=[System.IO.File]::ReadAllText($f); if($c -notmatch '\r\n'){$c=$c -replace '(?<!\r)\n','\r\n'; [System.IO.File]::WriteAllText($f,$c)}"
+:: Autofix LF->CRLF cherez env-peremennuyu (bez konflikta kavychek)
+set "_BAT=%~f0"
+powershell -Command "$f=$env:_BAT; $c=[System.IO.File]::ReadAllText($f); if($c -notmatch '\r\n'){$c=$c -replace '(?<!\r)\n','\r\n'; [System.IO.File]::WriteAllText($f,$c)}"
 
 echo ============================================
 echo  SONAR - Nizhegorodskaya oblast
