1268db37d413867801b8a4db1d5d2d074c42a4bc
WHO-AM-I-52 <toxacull@gmail.com>
2026-06-15 15:56:44 +0300

    feat(requests_app): регистрация admin_routes в Blueprint

--- requests_app/__init__.py
+++ requests_app/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 requests_bp = Blueprint('requests', __name__)
 
-from . import list_routes, form_routes, view_routes, action_routes, misc_routes  # noqa: E402,F401
+from . import list_routes, form_routes, view_routes, action_routes, admin_routes, misc_routes  # noqa: E402,F401
