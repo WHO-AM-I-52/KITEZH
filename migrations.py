ee6d219b4f66fec629fe3e51509e5b4e592744ed
WHO-AM-I-52 <toxacull@gmail.com>
2026-06-19 13:03:18 +0300

    feat(investmap): add investmap_classifiers, investmap_fields, investmap_rules tables with 116 fields

--- migrations.py
+++ migrations.py
@@ -125,6 +125,161 @@ CREATE INDEX IF NOT EXISTS idx_rc_request ON review_chain(request_id);
 """)
 
 
+def _migrate_investmap_tables(conn):
+    """Таблицы для investmap v2: classifiers, fields (116 атрибутов RF), rules."""
+    conn.executescript("""
+CREATE TABLE IF NOT EXISTS investmap_classifiers (
+    id             INTEGER PRIMARY KEY AUTOINCREMENT,
+    classifier_num INTEGER NOT NULL,
+    field_name     TEXT NOT NULL,
+    value          TEXT NOT NULL,
+    sort_order     INTEGER DEFAULT 0
+);
+CREATE TABLE IF NOT EXISTS investmap_fields (
+    id                 INTEGER PRIMARY KEY AUTOINCREMENT,
+    tech_name          TEXT NOT NULL UNIQUE,
+    display_name       TEXT NOT NULL,
+    data_type          TEXT NOT NULL DEFAULT 'text',
+    classifier_num     INTEGER,
+    is_required        INTEGER NOT NULL DEFAULT 0,
+    required_condition TEXT
+);
+CREATE TABLE IF NOT EXISTS investmap_rules (
+    id               INTEGER PRIMARY KEY AUTOINCREMENT,
+    source_field     TEXT NOT NULL,
+    source_value     TEXT NOT NULL,
+    target_field     TEXT NOT NULL,
+    recommended_text TEXT NOT NULL
+);
+""")
+    # INSERT OR IGNORE 116 атрибутов из atributy_investkarta_RF_26.09.2025.xlsx
+    fields = [
+        ('global_id', 'global_id', 'integer', None, 0, None),
+        ('system_object_id', 'Код во внешнем источнике', 'text', None, 0, None),
+        ('site_status_MAPS', 'Статус площадки', 'select', 606, 1, None),
+        ('method_of_sale_MAPS', 'Способ продажи', 'select', 607, 0, 'site_status_MAPS=Продана'),
+        ('other_method_of_sale_MAPS', 'Другой способ', 'text', None, 0, 'method_of_sale_MAPS=Другой способ'),
+        ('transaction_information_MAPS', 'Информация о сделке', 'select', 608, 0, 'site_status_MAPS=Продана OR Предоставлена в аренду'),
+        ('area_sold_site_MAPS', 'Площадь проданной площадки, га / кв.м', 'number', None, 0, 'transaction_information_MAPS=Продана часть площадки'),
+        ('sales_price_MAPS', 'Стоимость продажи, рублей', 'number', None, 0, 'transaction_information_MAPS=Продана часть площадки OR Продана целиком'),
+        ('investment_volume_for_project_MAPS', 'Объем инвестиций по проекту, рублей', 'number', None, 0, None),
+        ('form_use_remaining_area_MAPS', 'Форма использования оставшейся площади', 'select', 609, 0, 'transaction_information_MAPS=Продана часть площадки'),
+        ('other_form_use_remaining_area_MAPS', 'Другая форма использования', 'text', None, 0, 'form_use_remaining_area_MAPS=Другое'),
+        ('area_rented_site_MAPS', 'Площадь арендованной площадки, га / кв.м', 'number', None, 0, 'transaction_information_MAPS=Сдана в аренду часть площадки'),
+        ('rent_term_MAPS', 'Срок аренды, лет', 'integer', None, 0, 'transaction_information_MAPS=Сдана в аренду часть площадки'),
+        ('rental_cost_MAPS', 'Стоимость аренды, рублей в месяц', 'number', None, 0, 'transaction_information_MAPS=Сдана в аренду целиком OR Сдана в аренду часть площадки'),
+        ('project_cost_MAPS', 'Стоимость проекта, рублей', 'number', None, 0, None),
+        ('used_for_other_purposes_MAPS', 'Использована для других целей', 'select', 610, 0, 'site_status_MAPS=Использована для других целей'),
+        ('other_used_for_other_purposes_MAPS', 'Другая цель использования', 'text', None, 0, 'used_for_other_purposes_MAPS=Другое'),
+        ('reasons_withdrawal_from_sale_MAPS', 'Причины снятия с реализации', 'select', 611, 0, 'site_status_MAPS=Снята с реализации'),
+        ('other_reasons_withdrawal_from_sale_MAPS', 'Другая причина снятия', 'text', None, 0, 'reasons_withdrawal_from_sale_MAPS=Другое'),
+        ('site_name_MAPS', 'Название площадки', 'text', None, 1, None),
+        ('preferential_treatment_levels_MAPS', 'Преференциальный режим', 'select', 527, 1, None),
+        ('preferential_business_link_MAPS', 'Наименование объекта преференциального режима', 'link', None, 0, 'preferential_treatment_levels_MAPS!=Отсутствует'),
+        ('support_infrastructure_object_MAPS', 'Объект инфраструктуры поддержки', 'select', 467, 1, None),
+        ('support_infrastructure_link_MAPS', 'Наименование объекта инфраструктуры поддержки', 'link', None, 0, 'support_infrastructure_object_MAPS!=Без льгот'),
+        ('GISIP_link_MAPS', 'Индустриальные парки и промышленные технопарки из ГИСИП', 'link', None, 0, 'support_infrastructure_object_MAPS=Технопарк OR Индустриальный парк'),
+        ('region_dict_MAPS', 'Регион', 'select', 520, 1, None),
+        ('municipality_string_MAPS', 'Муниципальное образование', 'text', None, 1, None),
+        ('adress_object_MAPS', 'Адрес объекта', 'text', None, 1, None),
+        ('nearest_city_MAPS', 'Ближайший город', 'text', None, 1, None),
+        ('format_site_MAPS', 'Формат площадки', 'select', 405, 1, None),
+        ('type_site_MAPS', 'Тип площадки', 'select', 406, 1, None),
+        ('priority_site_dict_MAPS', 'Приоритетная площадка', 'select', 602, 0, None),
+        ('form_ownership_MAPS', 'Форма собственности объекта', 'select', 413, 1, None),
+        ('transaction_form_MAPS', 'Форма сделки', 'select', 408, 1, None),
+        ('cost_object_MAPS', 'Стоимость объекта, руб. (покупки или месячной аренды)', 'number', None, 0, None),
+        ('cost_per_ha_MAPS', 'Стоимость, руб./год за га', 'number', None, 0, None),
+        ('cost_per_sq_m_MAPS', 'Стоимость, руб./год за кв.м.', 'number', None, 0, None),
+        ('max_min_rental_year_MAPS', 'min и max сроки аренды, лет', 'text', None, 0, 'transaction_form_MAPS=Аренда OR Аренда через аукцион'),
+        ('procedure_determining_cost_str_MAPS', 'Порядок определения стоимости', 'text', None, 0, None),
+        ('hazard_class_object_MAPS', 'Класс опасности объекта', 'select', 409, 0, None),
+        ('characteristics_capital_buildings_MAPS', 'Характеристики объектов капитального строительства', 'text', None, 0, None),
+        ('land_area_MAPS', 'Свободная площадь ЗУ, га', 'number', None, 0, 'format_site_MAPS=Земельный участок'),
+        ('cadastral_land_number_MAPS', 'Кадастровый номер ЗУ', 'text', None, 0, 'format_site_MAPS=Земельный участок'),
+        ('types_authorized_use_MAPS', 'Варианты разрешенного использования', 'select', 415, 0, 'format_site_MAPS=Земельный участок'),
+        ('land_surveying_dic_MAPS', 'Межевание ЗУ', 'select', 518, 0, 'format_site_MAPS=Земельный участок'),
+        ('land_category_MAPS', 'Категория земель', 'select', 426, 0, 'format_site_MAPS=Земельный участок'),
+        ('area_property_complex_MAPS', 'Свободная площадь здания/сооружения/помещения, кв.м', 'number', None, 0, 'format_site_MAPS=Помещение OR Здания и сооружения'),
+        ('cadastral_property_complex_num_MAPS', 'Кадастровый номер здания/сооружения/помещения', 'text', None, 0, 'format_site_MAPS=Помещение OR Здания и сооружения'),
+        ('building_specifications_MAPS', 'Технические характеристики здания/сооружения/помещения', 'text', None, 0, None),
+        ('Name_owner_MAPS', 'Наименование собственника / администратора объекта', 'text', None, 1, None),
+        ('INN_MAPS', 'ИНН собственника', 'text', None, 0, None),
+        ('contact_person_MAPS', 'Контактное лицо', 'text', None, 0, None),
+        ('contact_phone_num_MAPS', 'Телефон контактного лица, e-mail', 'text', None, 1, None),
+        ('website_contact_person_MAPS', 'Сайт', 'text', None, 0, None),
+        ('notes_MAPS', 'Примечание', 'text', None, 0, None),
+        ('water_supply_availability_MAPS', 'Водоснабжение Наличие', 'select', 441, 1, None),
+        ('water_supply_tariff_consumption_MAPS', 'Водоснабжение Тариф на потребление, руб./куб.м', 'text', None, 0, 'water_supply_availability_MAPS=Да OR Возможно создание'),
+        ('water_supply_tariff_transportation_MAPS', 'Водоснабжение Тариф на транспортировку, руб./куб.м', 'text', None, 0, 'water_supply_availability_MAPS=Да'),
+        ('water_supply_available_capacity_MAPS', 'Водоснабжение Макс. допустимая мощность, куб.м/ч', 'number', None, 0, 'water_supply_availability_MAPS=Да'),
+        ('free_power_water_supply_MAPS', 'Водоснабжение Свободная мощность, куб.м/ч', 'number', None, 0, 'water_supply_availability_MAPS=Да'),
+        ('other_free_power_water_supply_MAPS', 'Водоснабжение Иные характеристики', 'text', None, 0, None),
+        ('bandwidth_water_supply_MAPS', 'Сети водоснабжения Пропускная способность, куб.м/ч', 'number', None, 0, 'water_supply_availability_MAPS=Да'),
+        ('water_disposal_availability_MAPS', 'Водоотведение Наличие', 'select', 441, 1, None),
+        ('water_disposal_tariff_consumption_MAPS', 'Водоотведение Тариф на потребление, руб./куб.м', 'text', None, 0, 'water_disposal_availability_MAPS=Да OR Возможно создание'),
+        ('water_disposal_tariff_transportation_MAPS', 'Водоотведение Тариф на транспортировку, руб./куб.м', 'text', None, 0, 'water_disposal_availability_MAPS=Да'),
+        ('water_disposal_available_capacity_MAPS', 'Водоотведение Макс. допустимая мощность, куб.м/ч', 'number', None, 0, 'water_disposal_availability_MAPS=Да'),
+        ('free_power_water_disposal_MAPS', 'Водоотведение Свободная мощность, куб.м/ч', 'number', None, 0, 'water_disposal_availability_MAPS=Да'),
+        ('other_free_power_water_disposal_MAPS', 'Водоотведение Иные характеристики', 'text', None, 0, None),
+        ('bandwidth_water_disposal_MAPS', 'Сети водоотведения Пропускная способность, куб.м/ч', 'number', None, 0, 'water_disposal_availability_MAPS=Да'),
+        ('gas_supply_availability_MAPS', 'Газоснабжение Наличие', 'select', 441, 1, None),
+        ('gas_supply_tariff_consumption_MAPS', 'Газоснабжение Тариф на потребление, руб./куб.м', 'text', None, 0, 'gas_supply_availability_MAPS=Да OR Возможно создание'),
+        ('gas_supply_tariff_transportation_MAPS', 'Газоснабжение Тариф на транспортировку, руб./куб.м', 'text', None, 0, 'gas_supply_availability_MAPS=Да'),
+        ('gas_supply_available_capacity_MAPS', 'Газоснабжение Макс. допустимая мощность, куб.м/ч', 'number', None, 0, 'gas_supply_availability_MAPS=Да OR Возможно создание'),
+        ('free_power_gas_supply_MAPS', 'Газоснабжение Свободная мощность, куб.м/ч', 'number', None, 0, 'gas_supply_availability_MAPS=Да'),
+        ('other_free_power_gas_supply_MAPS', 'Газоснабжение Иные характеристики', 'text', None, 0, None),
+        ('bandwidth_gas_supply_MAPS', 'Сети газоснабжения Пропускная способность, куб.м/ч', 'number', None, 0, 'gas_supply_availability_MAPS=Да'),
+        ('power_supply_availability_MAPS', 'Электроснабжение Наличие', 'select', 441, 1, None),
+        ('power_supply_tariff_consumption_MAPS', 'Электроснабжение Тариф на потребление, руб./МВт*ч', 'text', None, 0, 'power_supply_availability_MAPS=Да OR Возможно создание'),
+        ('power_supply_tariff_transportation_MAPS', 'Электроснабжение Тариф на транспортировку, руб./МВт*ч', 'text', None, 0, 'power_supply_availability_MAPS=Да'),
+        ('power_supply_available_capacity_MAPS', 'Электроснабжение Макс. допустимая мощность, МВт/ч', 'number', None, 0, 'power_supply_availability_MAPS=Да OR Возможно создание'),
+        ('free_power_electrosupply_MAPS', 'Электроснабжение Свободная мощность, МВт/ч', 'number', None, 0, 'power_supply_availability_MAPS=Да'),
+        ('other_free_power_electrosupply_MAPS', 'Электроснабжение Иные характеристики', 'text', None, 0, None),
+        ('bandwidth_electrosupply_MAPS', 'Сети электроснабжения Пропускная способность, МВт/ч', 'number', None, 0, 'power_supply_availability_MAPS=Да'),
+        ('heat_supply_availability_MAPS', 'Теплоснабжение Наличие', 'select', 441, 1, None),
+        ('heat_supply_tariff_consumption_MAPS', 'Теплоснабжение Тариф на потребление, руб./Гкал*ч', 'text', None, 0, 'heat_supply_availability_MAPS=Да OR Возможно создание'),
+        ('heat_supply_tariff_transportation_MAPS', 'Теплоснабжение Тариф на транспортировку, руб./Гкал*ч', 'text', None, 0, 'heat_supply_availability_MAPS=Да'),
+        ('heat_supply_available_capacity_MAPS', 'Теплоснабжение Макс. допустимая мощность, Гкал/ч', 'number', None, 0, 'heat_supply_availability_MAPS=Да OR Возможно создание'),
+        ('free_power_heat_supply_MAPS', 'Теплоснабжение Свободная мощность, Гкал/ч', 'number', None, 0, 'heat_supply_availability_MAPS=Да'),
+        ('other_free_power_heat_supply_MAPS', 'Теплоснабжение Иные характеристики', 'text', None, 0, None),
+        ('bandwidth_heat_supply_MAPS', 'Сети теплоснабжения Пропускная способность, Гкал/ч', 'number', None, 0, 'heat_supply_availability_MAPS=Да'),
+        ('MSW_removal_availability_MAPS', 'Вывоз ТКО Наличие', 'select', 441, 1, None),
+        ('MSW_removal_tariff_MAPS', 'Вывоз ТКО Тариф, руб./тонна', 'number', None, 0, None),
+        ('MSW_removal_tariff_2_MAPS', 'Вывоз ТКО Тариф, руб./куб.м', 'number', None, 0, None),
+        ('access_roads_availability_MAPS', 'Наличие подъездных путей', 'select', 441, 1, None),
+        ('railway_availability_MAPS', 'Наличие ж/д', 'select', 441, 1, None),
+        ('truck_parking_availability_MAPS', 'Наличие парковки грузового транспорта', 'select', 441, 1, None),
+        ('access_roads_other_MAPS', 'Транспорт Иные характеристики', 'text', None, 0, None),
+        ('description_application_procedure_MAPS', 'Описание процедуры подачи заявки', 'text', None, 1, None),
+        ('list_of_documents_for_application_MAPS', 'Перечень документов для подачи заявки', 'text', None, 0, None),
+        ('Email_address_for_applying_MAPS', 'Адрес эл. почты для подачи заявки', 'text', None, 0, None),
+        ('link_to_application_form_MAPS', 'Ссылка на форму подачи заявки', 'text', None, 0, None),
+        ('list_economic_activities_for_implementation_dict_MAPS', 'Виды экономической деятельности на площадке', 'select', 513, 1, None),
+        ('urban_plan_characteristics_and_limits_MAPS', 'Градостроительные характеристики и ограничения', 'text', None, 0, None),
+        ('territorial_plan_documents_file_MAPS', 'Документы территориального планирования', 'file', None, 0, None),
+        ('other_information_site_MAPS', 'Иные сведения', 'text', None, 0, None),
+        ('photos_object_MAPS', 'Фотографии объекта', 'file', None, 0, None),
+        ('documents_object_MAPS', 'Документы по объекту', 'file', None, 0, None),
+        ('flag_MAIP_MAPS', 'Наличие МАИП', 'bool', None, 0, None),
+        ('description_of_benefits_MAPS', 'Описание льготы', 'text', None, 0, 'flag_MAIP_MAPS=1'),
+        ('geodata', 'Геоданные (координаты точки/линии/полигона)', 'geo', None, 0, None),
+        ('coordinate_flag_MAPS', 'Не могу отметить координаты на карте', 'bool', None, 1, None),
+        ('object_geotype_SubRegIP', 'Геотип объекта', 'select', 439, 0, 'coordinate_flag_MAPS=1'),
+        ('latitude_MAPS', 'Широта объекта в координатах WGS-84', 'number', None, 0, 'object_geotype_SubRegIP=точка'),
+        ('longitude_MAPS', 'Долгота объекта в координатах WGS-84', 'number', None, 0, 'object_geotype_SubRegIP=точка'),
+        ('line_coordinates_SubRegIP', 'Набор координат линии в WGS-84', 'text', None, 0, 'object_geotype_SubRegIP=линия'),
+        ('polygon_coordinates_SubRegIP', 'Набор координат полигона в WGS-84', 'text', None, 0, 'object_geotype_SubRegIP=полигон'),
+    ]
+    for row in fields:
+        conn.execute(
+            "INSERT OR IGNORE INTO investmap_fields "
+            "(tech_name, display_name, data_type, classifier_num, is_required, required_condition) "
+            "VALUES (?,?,?,?,?,?)",
+            row
+        )
+
+
 def init_db():
     conn = sqlite3.connect(DB_PATH, timeout=15)
     conn.row_factory = sqlite3.Row
@@ -272,6 +427,9 @@ CREATE TABLE IF NOT EXISTS settings (key TEXT PRIMARY KEY, value TEXT NOT NULL);
         # ── review_chain ─────────────────────────────────────────────────────────────
         _migrate_review_chain_table(conn)
 
+        # ── investmap v2 ─────────────────────────────────────────────────────────────
+        _migrate_investmap_tables(conn)
+
         # ── Миграция requests ──────────────────────────────────────────────────────
         cols = {r[1] for r in conn.execute("PRAGMA table_info(requests)").fetchall()}
         for col in ['source_type', 'request_files', 'edit_reason', 'updated_by']:
@@ -339,6 +497,9 @@ def migrate_db():
 
         # ── review_chain ─────────────────────────────────────────────────────────────
         _migrate_review_chain_table(conn)
+
+        # ── investmap v2 ─────────────────────────────────────────────────────────────
+        _migrate_investmap_tables(conn)
 
         # ── requests ──────────────────────────────────────────────────────────────────────────
         cols = {r[1] for r in conn.execute("PRAGMA table_info(requests)").fetchall()}
